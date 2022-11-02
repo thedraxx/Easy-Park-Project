13b6e8c16bac3bd47b56fc997c438724fb5d5539
thedraxx <thedraxxgames@gmail.com>
2022-11-02 13:07:06 -0300

    eliminacion archivos residuales front

--- FrontEnd/Provider/php/ConectionDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-     define('DB_HOST','bp6nfzavyucdmj07us1w-mysql.services.clever-cloud.com');
-     define('DB_USUARIO','ukfwnbqeu0ysoyct');
-     define('DB_CONTRA', 'TljBTzMktwbU4NwhYbj7');
-     define('DB_NOMBRE', 'bp6nfzavyucdmj07us1w');
-     define('DB,CHARSET', 'utf8')
-?>
--- FrontEnd/Provider/php/crearFecha copy.php
+++
@@ -1,9 +0,0 @@
-<?php
-//OBTENER FECHA ACTUAL 
-function getDatetimeNow() {
-    $tz_object = new DateTimeZone('America/Argentina/Buenos_Aires');
-    $datetime = new DateTime();
-    $datetime->setTimezone($tz_object);
-    return $datetime->format('Y\-m\-d\ h:i:s');
-}
-?>
